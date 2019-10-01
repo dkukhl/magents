943c0f506b057b46f20cbaccc1829ab79fbf6f65
dkukhl <dkukhl@yandex.ru>
2019-10-01 19:56:45 +0300

    barcodes sql

--- sql/barcodes.sql
+++ sql/barcodes.sql
@@ -1,17 +1,10 @@
 SELECT
-  barcode,
-  price,
-  pos.title as pos_title
+  pos.title as pos_title,
+  ARRAY_AGG(ARRAY[reports.barcode::varchar(30),  reports.price::varchar(30)]::text) as barcodes
 
 FROM
   reports
 INNER JOIN pos ON reports.pos_id = pos.id
 
 GROUP BY
-  barcode, price, pos.title
-
-HAVING
-  COUNT(pos.id) > 1;
-
--- Не совсем понял задание, в данном случае выбираются все штрихкоды 
--- и цены из точек продаж с дублирующимся названием
+  pos_title;
